3ec0a48d88e89814ea4da28df004a571684efc13
Arittro7 <arittro007@gmail.com>
2024-07-15 12:39:39 +0600

    book card fixed

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,43 +1,30 @@
-import { CiStar } from "react-icons/ci";
+import { FaRegStar } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
 
 const Book = ({book}) => {
   const { bookName, authorName, image, rating, category, tags,id } = book;
   return (
     <Link to={`/book/${id}`}>
-      <div className="card card-compact bg-base-100 shadow-lg">
-        <div className="bg-purple-100 w-70 h-70 border rounded shadow-lg m-4 ">
-          <figure className="p-6">
-            <img src={image} alt="card_img" />
-          </figure>
-        </div>
-        <p className="flex gap-4 p-2">
-          <p className=" text-[#23BE0A] bg-[#23BE0A0D] border rounded-full w-24">
-            {tags[0]}
-          </p>
-          <p className="text-[#23BE0A] bg-[#23BE0A0D] border rounded-full w-28">
-            {tags[1]}
-          </p>
-        </p>
-        <div className="card-body font-bold text-base">
-          <h2 className="card-title text-2xl">{bookName}</h2>
-        </div>
-        <h2 className="p-4 text-base font-medium work_sens">
-          By : {authorName}
-        </h2>
-        <div className="p-4">
-          <p className="border-t border-dashed  border-[#13131326]"></p>
-        </div>
-        <div className="flex justify-between p-4 font-sm text-base ">
-          <p>{category}</p>
-          <p className="flex gap-1 items-center">
-            {rating}
-            <CiStar></CiStar>
-          </p>
-        </div>
+      <div className="p-5 max-h-[500px] flex-col border-2 rounded-xl space-y-2 mt-9">
+      <img className="w-48 mx-auto p-4" src={image} alt="" />
+      <div className="flex gap-3">
+      <p className="text-xl text-green-600">{tags[0]}</p>
+      <p className="text-xl text-green-600">{tags[1]}</p>
       </div>
+      <h2 className="text-2xl font-bold">{bookName}</h2>
+      <h2 className="text-xl">{authorName}</h2>
+      <div className="flex justify-between text-xl">
+      <h2 className="text-xl text-green-600">{category}</h2>
+      <p className="flex gap-2">{rating}<span><FaRegStar className="text-orange-500"></FaRegStar></span></p>
+      </div>
+    </div>
     </Link>
   );
 };
+
+Book.propTypes={
+  book: PropTypes.object
+}
 
 export default Book;
